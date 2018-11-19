d5606ae67b3264588f404622905f478614d27f1f
Jakub Raban <jakub.raban@gmail.com>
2018-11-19 23:45:24 +0100

    Added case reference

---
+++ src/main/java/pl/jakubraban/whereismyjudgement/CourtCaseReference.java
@@ -0,0 +1,11 @@
+package pl.jakubraban.whereismyjudgement;
+
+import java.util.List;
+
+public class CourtCaseReference {
+
+    private String caseNumber;
+    private List<String> judgementIDs;
+    private boolean generated;
+
+}
