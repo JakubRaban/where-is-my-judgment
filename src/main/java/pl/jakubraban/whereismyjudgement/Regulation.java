abbf75040a71a1f40b4aa0203084c273e5b43287
Jakub Raban <jakub.raban@gmail.com>
2018-11-19 23:39:31 +0100

    Added regulation

--- src/main/java/pl/jakubraban/whereismyjudgement/Regulation.java
+++ src/main/java/pl/jakubraban/whereismyjudgement/Regulation.java
@@ -1,4 +1,11 @@
 package pl.jakubraban.whereismyjudgement;
 
 public class Regulation {
+
+    private String journalTitle;
+    private int journalYear;
+    private int journalNumber;
+    private int journalEntry;
+    private String regulationText;
+
 }
