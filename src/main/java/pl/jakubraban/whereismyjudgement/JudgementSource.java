511b954124ab3a072d1c765e609e06a26fd79710
Jakub Raban <jakub.raban@gmail.com>
2018-11-19 23:34:26 +0100

    Added judgement source

--- src/main/java/pl/jakubraban/whereismyjudgement/JudgementSource.java
+++ src/main/java/pl/jakubraban/whereismyjudgement/JudgementSource.java
@@ -1,4 +1,14 @@
 package pl.jakubraban.whereismyjudgement;
 
+import java.util.Calendar;
+
 public class JudgementSource {
+
+    private CourtType sourceJudicalSystem;
+    private String judgementURL;
+    private int sourceJudgementID;
+    private String publisherName;
+    private String revisorName;
+    private Calendar publicationDate;
+
 }
