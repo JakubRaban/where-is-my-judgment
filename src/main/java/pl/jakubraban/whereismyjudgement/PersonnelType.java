c1ea7f9fb8ae45e055c6c1c4eda0f25c037ab608
Jakub Raban <jakub.raban@gmail.com>
2018-11-20 09:33:40 +0100

    Added enum personnel type

---
+++ src/main/java/pl/jakubraban/whereismyjudgement/PersonnelType.java
@@ -0,0 +1,8 @@
+package pl.jakubraban.whereismyjudgement;
+
+public enum PersonnelType {
+
+    ONE_PERSON, THREE_PERSON, FIVE_PERSON, SEVEN_PERSON,
+    ALL_COURT, ALL_CHAMBER, JOINED_CHAMBERS;
+
+}
