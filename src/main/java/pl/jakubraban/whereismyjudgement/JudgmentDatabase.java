86a4dc791f4620960da5315fdfafaeee4283dcb8
Jakub Raban <jakub.raban@gmail.com>
2018-11-20 21:16:05 +0100

    Sketch of JudgmentDatabase class

---
+++ src/main/java/pl/jakubraban/whereismyjudgement/JudgmentDatabase.java
@@ -0,0 +1,15 @@
+package pl.jakubraban.whereismyjudgement;
+
+import java.util.Map;
+
+public class JudgmentDatabase {
+
+    private Map<String, Judgment> judgments;
+
+    public void addToDatabase(Judgment judgment) {
+
+    }
+
+
+
+}
