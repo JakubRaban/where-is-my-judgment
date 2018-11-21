77bbface1615d88391065b87f6ef98b07743acba
Jakub Raban <jakub.raban@gmail.com>
2018-11-21 18:28:57 +0100

    Created Judgment Parser

---
+++ src/main/java/pl/jakubraban/whereismyjudgement/JudgmentParser.java
@@ -0,0 +1,26 @@
+package pl.jakubraban.whereismyjudgement;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.LinkedList;
+
+public class JudgmentParser {
+
+    public LinkedList<Judgment> parse(String json) {
+        String properJson = dropMetadata(json);
+        System.out.println(properJson.substring(0,100));
+        Type judgmentListType = new TypeToken<LinkedList<Judgment>>(){}.getType();
+        LinkedList<Judgment> judgments = new Gson().fromJson(properJson, judgmentListType);
+        Judgment one = judgments.get(0);
+        return judgments;
+    }
+
+    private String dropMetadata(String parsedJson) {
+        int start = parsedJson.indexOf("items") + 7;
+        int end = parsedJson.indexOf("queryTemplate") - 2;
+        return parsedJson.substring(start, end);
+    }
+
+}
