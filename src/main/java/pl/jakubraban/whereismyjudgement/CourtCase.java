fe66e728a01fe2a8131f818b6f1c715e062465c9
Jakub Raban <jakub.raban@gmail.com>
2018-11-21 18:28:07 +0100

    CaseNumber class

---
+++ src/main/java/pl/jakubraban/whereismyjudgement/CourtCase.java
@@ -0,0 +1,11 @@
+package pl.jakubraban.whereismyjudgement;
+
+public class CourtCase {
+
+    private String caseNumber;
+
+    public String getCaseNumber() {
+        return caseNumber;
+    }
+
+}
