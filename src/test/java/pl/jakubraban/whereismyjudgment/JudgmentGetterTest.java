3c2010a5d153143284eb193f366fd5e7158e1b85
Jakub Raban <jakub.raban@gmail.com>
2018-11-21 14:25:05 +0100

    Test class for JudgmentGetter

---
+++ src/test/java/pl/jakubraban/whereismyjudgment/JudgmentGetterTest.java
@@ -0,0 +1,22 @@
+package pl.jakubraban.whereismyjudgment;
+
+import org.junit.Test;
+import pl.jakubraban.whereismyjudgement.JudgmentGetter;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class JudgmentGetterTest {
+
+    @Test
+    public void testGetAllJSONs() throws IOException {
+        var getter = new JudgmentGetter(System.getProperty("user.dir") + "\\judgments\\dummy");
+        List<Path> paths = getter.getAllJSONs();
+        assertEquals(10, paths.size());
+        System.out.println(paths);
+    }
+
+}
