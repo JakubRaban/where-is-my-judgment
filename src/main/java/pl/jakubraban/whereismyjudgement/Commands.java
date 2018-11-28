06a65152fbb3dc424ab848341a6599a7db641184
Jakub Raban <jakub.raban@gmail.com>
2018-11-28 16:45:04 +0100

    Added fuction for showing number of judgments per each month and modified getTopNJudges() so to return a map

--- src/main/java/pl/jakubraban/whereismyjudgement/Commands.java
+++ src/main/java/pl/jakubraban/whereismyjudgement/Commands.java
@@ -1,12 +1,16 @@
 package pl.jakubraban.whereismyjudgement;
 
 import pl.jakubraban.whereismyjudgement.data.judge.Judge;
+import pl.jakubraban.whereismyjudgement.data.judgment.CourtType;
 import pl.jakubraban.whereismyjudgement.data.judgment.Judgment;
 import pl.jakubraban.whereismyjudgement.storage.*;
 
+import java.time.Month;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.reverseOrder;
 
 public class Commands {
 
@@ -21,7 +25,6 @@ public class Commands {
         for(String signature : signatures) {
             judgments.add(database.search(signature));
         }
-
         return judgments.stream()
                 .map(Judgment::getMetric)
                 .collect(Collectors.toList());
@@ -37,21 +40,36 @@ public class Commands {
                 .count();
     }
 
-    public List<Judge> getTopNJudges(final int N) {
+    public Map<Judge, Integer> getTopNJudges(final int N) {
         Map<Judge, Integer> judgeCount = new HashMap<>();
+        Map<Judge, Integer> topJudges = new HashMap<>();
         getJudgeStream()
                 .forEach(judge -> judgeCount.merge(judge, 1, (a,b) -> a + b));
-        return judgeCount.entrySet().stream()
-                .sorted(Comparator.comparing(Map.Entry::getValue))
+        judgeCount.entrySet().stream()
+                .sorted(comparing(Map.Entry::getValue, reverseOrder()))
                 .limit(N)
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
+                .forEach(entry -> topJudges.put(entry.getKey(), entry.getValue()));
+        return topJudges;
+    }
+
+    public Map<Month, Integer> numberOfJudgmentsByMonth() {
+        Map<Month, Integer> judgmentsByMonth = new HashMap<>();
+        getJudgmentsStream()
+                .map(Judgment::getJudgmentDate)
+                .map(calendar -> calendar.get(Calendar.MONTH))
+                .map(Month::of)
+                .forEach(month -> judgmentsByMonth.merge(month, 1, (a,b) -> a + b));
+        return judgmentsByMonth;
     }
 
     private Stream<Judge> getJudgeStream() {
-        return database.getAllJudgments().stream()
+        return getJudgmentsStream()
                 .map(Judgment::getJudges)
                 .flatMap(List::stream);
+    }
+
+    private Stream<Judgment> getJudgmentsStream() {
+        return database.getAllJudgments().stream();
     }
 
 }
