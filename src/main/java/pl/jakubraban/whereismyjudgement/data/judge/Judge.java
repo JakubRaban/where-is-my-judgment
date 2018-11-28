dfbc877b277e1e71822c430e9fe43306195df54c
Jakub Raban <jakub.raban@gmail.com>
2018-11-28 15:36:38 +0100

    Added Judge class hash function so it can be placed as key in a map

--- src/main/java/pl/jakubraban/whereismyjudgement/data/judge/Judge.java
+++ src/main/java/pl/jakubraban/whereismyjudgement/data/judge/Judge.java
@@ -1,6 +1,7 @@
 package pl.jakubraban.whereismyjudgement.data.judge;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Judge {
 
@@ -28,4 +29,18 @@ public class Judge {
         return sb.toString();
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Judge)) return false;
+        Judge judge = (Judge) o;
+        return Objects.equals(name, judge.name) &&
+                Objects.equals(function, judge.function) &&
+                Objects.equals(specialRoles, judge.specialRoles);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, function, specialRoles);
+    }
 }
