c1ee5e9d7d1a26bb7400ef97ff77eb64058a7153
Jakub Raban <jakub.raban@gmail.com>
2018-11-29 15:16:35 +0100

    Added DataPrinter

---
+++ src/main/java/pl/jakubraban/whereismyjudgement/output/DataPrinter.java
@@ -0,0 +1,25 @@
+package pl.jakubraban.whereismyjudgement.output;
+
+import java.util.*;
+
+public class DataPrinter {
+
+    public void print(Object o) {
+        if(o instanceof Map) printMap((Map) o);
+        if(o instanceof List) {
+            for(Object s : (List) o) {
+                System.out.println(s);
+                System.out.println();
+            }
+        }
+        if(o instanceof String) System.out.println((String) o);
+        else System.out.println(o);
+    }
+
+    private void printMap(Map map) {
+        for(Map.Entry entry : ((Map<Object, Object>) map).entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
+
+}
