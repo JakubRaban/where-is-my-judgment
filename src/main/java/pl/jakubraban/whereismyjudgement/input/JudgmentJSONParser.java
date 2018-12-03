e72127ef5a41b740e68298e23f69b7eab7eef85a
Jakub Raban <jakub.raban@gmail.com>
2018-12-03 14:15:09 +0100

    Fixed an issue where incorrect judgment JSON was recognised as correct and vice versa

--- src/main/java/pl/jakubraban/whereismyjudgement/input/JudgmentJSONParser.java
+++ src/main/java/pl/jakubraban/whereismyjudgement/input/JudgmentJSONParser.java
@@ -35,14 +35,10 @@ public class JudgmentJSONParser {
         }
     }
 
-    private boolean isJSONFile(String json) {
-        return json.contains("items") && json.contains("queryTemplate");
-    }
-
     private String dropMetadata(String parsedJson) throws ParseException {
         int start = parsedJson.indexOf("items") + 7;
         int end = parsedJson.indexOf("queryTemplate") - 2; // TODO wyjątek kiedy JSON to nie wyrok
-        if(start == -1 || end == -1) throw new ParseException("The file is not a proper judgment JSON", 0);
+        if(start == 6 || end == -3) throw new ParseException("The file is not a proper judgment JSON", 0);
         return parsedJson.substring(start, end);
     }
 
