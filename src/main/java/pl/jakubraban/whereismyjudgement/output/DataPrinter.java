61d37b592c875bbd697e48c7987093040b056d4a
Jakub Raban <jakub.raban@gmail.com>
2018-12-04 11:45:30 +0100

    Added printing HashMaps in parallel to LinkedHashMaps

--- src/main/java/pl/jakubraban/whereismyjudgement/output/DataPrinter.java
+++ src/main/java/pl/jakubraban/whereismyjudgement/output/DataPrinter.java
@@ -1,6 +1,7 @@
 package pl.jakubraban.whereismyjudgement.output;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +10,7 @@ public class DataPrinter {
 
     public void print(Object o) throws IOException {
         if(o instanceof LinkedHashMap) printMap((LinkedHashMap) o);
+        else if(o instanceof HashMap) printMap ((HashMap) o);
         else if(o instanceof List) {
             for(Object s : (List) o) {
                 System.out.println(s.toString());
@@ -22,6 +24,12 @@ public class DataPrinter {
 
     private void printMap(LinkedHashMap map) throws IOException {
         for(Map.Entry entry : ((LinkedHashMap<Object, Object>) map).entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
+
+    private void printMap(HashMap map) {
+        for(Map.Entry entry : ((HashMap<Object, Object>) map).entrySet()) {
             System.out.println(entry.getKey() + " - " + entry.getValue());
         }
     }
