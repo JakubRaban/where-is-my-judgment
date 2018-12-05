f82e8bd3e394930ec717cf944a0ba6a440ab0bd3
Jakub Raban <jakub.raban@gmail.com>
2018-12-05 11:54:59 +0100

    FunctionResult class

---
+++ src/main/java/pl/jakubraban/whereismyjudgement/FunctionResult.java
@@ -0,0 +1,33 @@
+package pl.jakubraban.whereismyjudgement;
+
+import java.util.List;
+
+public class FunctionResult {
+
+    private Object result;
+    private List<String> erroneousInput;
+    private String affectedClass;
+
+    FunctionResult(Object result) {
+        this.result = result;
+    }
+
+    FunctionResult(Object result, List<String> erroneousInput, String affectedClass) {
+        this.result = result;
+        this.erroneousInput = erroneousInput;
+        this.affectedClass = affectedClass;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public List<String> getErroneousInput() {
+        return erroneousInput;
+    }
+
+    public String getAffectedClass() {
+        return affectedClass;
+    }
+
+}
