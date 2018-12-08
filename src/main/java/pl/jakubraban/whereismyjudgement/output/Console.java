96b043716ffa2e56840b1161637d5348952aed2c
Jakub Raban <jakub.raban@gmail.com>
2018-12-08 18:17:09 +0100

    FuctionResultFormatter -> FormattableFunctionResult and now inherits from FuntionResult

--- src/main/java/pl/jakubraban/whereismyjudgement/output/Console.java
+++ src/main/java/pl/jakubraban/whereismyjudgement/output/Console.java
@@ -1,27 +1,24 @@
 package pl.jakubraban.whereismyjudgement.output;
 
-import pl.jakubraban.whereismyjudgement.functions.FunctionResult;
-import pl.jakubraban.whereismyjudgement.Functions;
+import pl.jakubraban.whereismyjudgement.functions.FunctionInvoker;
 import pl.jakubraban.whereismyjudgement.input.CommandParser;
-import pl.jakubraban.whereismyjudgement.storage.JudgmentDatabase;
-import pl.jakubraban.whereismyjudgement.storage.JudgmentDatabaseProvider;
 
 import java.io.IOException;
 import java.nio.file.InvalidPathException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Console {
 
-    private Functions functions = new Functions();
-    private CommandParser commandParser = new CommandParser(functions);
-    private FunctionResultFormatter formatter = new FunctionResultFormatter();
-    private JudgmentDatabase database = JudgmentDatabaseProvider.getDatabase();
+    private CommandParser commandParser = new CommandParser();
+    private FunctionInvoker invoker = new FunctionInvoker();
     private Scanner sc = new Scanner(System.in);
 
     public void initialize(String judgmentsPath) {
         try {
-            FunctionResult fileLoadingResult = functions.getNewJudgments(judgmentsPath);
-            System.out.println("Załadowano wyroków: " + fileLoadingResult.getResult());
+            FormattableFunctionResult fileLoadingResult =
+                    new FormattableFunctionResult(invoker.invoke("setPath", judgmentsPath));
+            System.out.println(fileLoadingResult.format());
         } catch(InvalidPathException ipe) {
             System.out.println("Podana jako parametr ścieżka nie jest katalogiem");
         } catch(IOException ioe) {
@@ -37,14 +34,15 @@ public class Console {
             try {
                 System.out.print("?> ");
                 String command = sc.nextLine();
-                FunctionResult result = commandParser.parse(command);
-                String formattedResult = formatter.format(result);
+                FormattableFunctionResult result = new FormattableFunctionResult(commandParser.parse(command));
+                String formattedResult = result.format();
                 System.out.println(formattedResult);
             } catch (IOException ioe) {
                 System.out.println("Błąd odczytu z pliku");
             } catch (IllegalArgumentException iae) {
-                if(iae.getMessage().equals("Bad command")) System.out.println("Złe polecenie");
-                else System.out.println("Nieprawidłowy argument lub ilość argumentów");
+                System.out.println("Nieprawidłowy argument lub ilość argumentów");
+            } catch(NoSuchElementException nsee) {
+                System.out.println("Złe polecenie");
             } finally {
                 System.out.println();
             }
---
+++ src/main/java/pl/jakubraban/whereismyjudgement/output/FormattableFunctionResult.java
@@ -0,0 +1,52 @@
+package pl.jakubraban.whereismyjudgement.output;
+
+import pl.jakubraban.whereismyjudgement.functions.FunctionResult;
+import pl.jakubraban.whereismyjudgement.Utilities;
+
+import java.util.*;
+
+public class FormattableFunctionResult extends FunctionResult {
+
+    public FormattableFunctionResult(FunctionResult result) {
+        super(result.getResult(), result.getErroneousInput(), result.getErrorAffectedClassName(), result.getMessage());
+    }
+
+    public String format() {
+        if (this == FunctionResult.NONE) return "";
+        Optional<Object> objectToPrint = Optional.ofNullable(getResult());
+        StringBuilder printedResult = new StringBuilder();
+        if (objectToPrint.isPresent()) printedResult.append(format(objectToPrint.orElseThrow()));
+        if (!getMessage().isEmpty()) printedResult.append(getMessage());
+        if (!getErroneousInput().isEmpty()) appendErrorMessage(printedResult);
+        return printedResult.toString();
+    }
+
+    private String getFormattedMap(HashMap map) {
+        StringBuilder sb = new StringBuilder();
+        for(Map.Entry entry : ((HashMap<Object, Object>) map).entrySet()) {
+            sb.append(entry.getKey()).append(" -- ").append(entry.getValue()).append("\n");
+        }
+        return sb.toString();
+    }
+
+    private String format(Object o) {
+        if(o instanceof HashMap) return getFormattedMap((HashMap) o);
+        else if(o instanceof List) {
+            StringBuilder sb = new StringBuilder();
+            for(Object s : (List) o) {
+                sb.append(s.toString());
+            }
+            return sb.toString();
+        }
+        else if(o instanceof String) return (String) o;
+        else return o.toString();
+    }
+
+    private void appendErrorMessage(StringBuilder printedResult) {
+        printedResult.append("BŁĄD: Żadna instancja ").append(getErrorAffectedClassName())
+                .append(" nie wynika z użycia nazw(y) ")
+                .append(Utilities.getListWithCommas(getErroneousInput()))
+                .append("\n");
+    }
+
+}
