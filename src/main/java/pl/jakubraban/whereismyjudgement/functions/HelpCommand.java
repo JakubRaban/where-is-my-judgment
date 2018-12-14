96d4a95424ee6d0abb97e5b5f1b8cba64d3b6584
Jakub Raban <jakub.raban@gmail.com>
2018-12-14 23:23:05 +0100

    Added option to show help just for one command

--- src/main/java/pl/jakubraban/whereismyjudgement/functions/HelpCommand.java
+++ src/main/java/pl/jakubraban/whereismyjudgement/functions/HelpCommand.java
@@ -2,9 +2,11 @@ package pl.jakubraban.whereismyjudgement.functions;
 
 import pl.jakubraban.whereismyjudgement.Utilities;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class HelpCommand extends AbstractFunction {
 
@@ -15,10 +17,20 @@ public class HelpCommand extends AbstractFunction {
     @Override
     FunctionResult invoke(String... args) {
         Map<String, AbstractFunction> functions = new FunctionInvoker().getFunctionNames();
-        List<String> help = functions.values().stream()
+        Stream<AbstractFunction> functionStream = functions.values().stream();
+        if(args.length > 0) {
+            try {
+                return new FunctionResult(null, functionStream
+                        .filter(function -> function.getName().toLowerCase().equals(args[0]))
+                        .map(AbstractFunction::getHelpMessage)
+                        .collect(Collectors.toList()).get(0));
+            } catch (Exception e) {
+                return new FunctionResult(null, Collections.singletonList(args[0]), "Function", "");
+            }
+        }
+        else return new FunctionResult(null, Utilities.getDelimitedList(functionStream
                 .map(AbstractFunction::getHelpMessage)
-                .collect(Collectors.toList());
-        return new FunctionResult(null, Utilities.getDelimitedList(help, '\n'));
+                .collect(Collectors.toList()), '\n'));
     }
 
     @Override
