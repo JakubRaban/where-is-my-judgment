cb3d303c3623a27859f7a26eede9e9824d5cd10a
Jakub Raban <jakub.raban@gmail.com>
2018-12-19 12:11:12 +0100

    Fixed exception when JSON is malformed

--- src/main/java/pl/jakubraban/whereismyjudgement/input/JudgmentJSONParser.java
+++ src/main/java/pl/jakubraban/whereismyjudgement/input/JudgmentJSONParser.java
@@ -3,6 +3,7 @@ package pl.jakubraban.whereismyjudgement.input;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 import pl.jakubraban.whereismyjudgement.data.judgment.Judgment;
 
@@ -14,22 +15,25 @@ import java.util.LinkedList;
 
 public class JudgmentJSONParser {
 
-    public LinkedList<Judgment> parse(String json) throws ParseException {
-        String properJson = dropMetadata(json);
-        Type judgmentListType = new TypeToken<LinkedList<Judgment>>() {
-        }.getType();
-        Gson gson = new GsonBuilder().registerTypeAdapter(Calendar.class, (JsonDeserializer<Calendar>) (element, arg1, arg2) -> {
-            String date = element.getAsString();
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-            try {
-                Calendar cal = Calendar.getInstance();
-                cal.setTime(sdf.parse(date));
-                return cal;
-            } catch (ParseException e) {
-                return null;
-            }
-        }).create();
-        return gson.fromJson(properJson, judgmentListType);
+    public LinkedList<Judgment> parse(String json)  {
+        try {
+            String properJson = dropMetadata(json);
+            Type judgmentListType = new TypeToken<LinkedList<Judgment>>() {}.getType();
+            Gson gson = new GsonBuilder().registerTypeAdapter(Calendar.class, (JsonDeserializer<Calendar>) (element, arg1, arg2) -> {
+                String date = element.getAsString();
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                try {
+                    Calendar cal = Calendar.getInstance();
+                    cal.setTime(sdf.parse(date));
+                    return cal;
+                } catch (ParseException e) {
+                    return null;
+                }
+            }).create();
+            return gson.fromJson(properJson, judgmentListType);
+        } catch (ParseException | JsonSyntaxException e) {
+            return new LinkedList<>();
+        }
     }
 
     private String dropMetadata(String parsedJson) throws ParseException {
